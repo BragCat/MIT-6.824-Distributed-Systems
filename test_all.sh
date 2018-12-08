96655cd1fbc1b7f24dc8074a1d8a2c03f98e97a9
bragcat <bragcat.li@gmail.com>
2018-12-08 20:10:22 +0800

    Test: add test_all script to test all passed tests

---
+++ test_all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export GOPATH=${PWD}
+test_dir=("mapreduce" "raft" "kvraft" "shardmaster")
+for dir_name in ${test_dir[@]};
+do
+    echo "Test in "${dir_name}
+    current_dir=${GOPATH}/src/${dir_name}
+    cd ${current_dir}
+    go test
+    echo
+done
+
